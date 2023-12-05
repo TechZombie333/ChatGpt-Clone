54de2607c326385554c326e156799617ad321b32
Kundan <100273989+InfiniteCoder100@users.noreply.github.com>
2023-12-05 19:27:20 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import gptLogo from "./assets/chatgptLogo.svg";
+import message from "./assets/message.svg";
+import home from "./assets/home.svg";
+import saved from "./assets/bookmark.svg";
+import upgrade from "./assets/rocket.svg";
+import addBtn from "./assets/add-30.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sideBar">
+        <div className="upperSide">
+          <div className="upperSideTop">
+            <img src={gptLogo} alt="logo" className="logo" />
+            <span className="brand">ChatGpt</span>
+          </div>
+          <button className="midBtn">
+            <img src={addBtn} alt="addBtn" className="addBtn" />
+            New Chat
+          </button>
+          <div className="upperSideBottom">
+            <div className="q1">
+              <button className="query">
+                <img src={message} alt="query" />
+                What is Programming ?
+              </button>
+            </div>
+            <div className="q2">
+              <button className="query">
+                <img src={message} alt="query" />
+                How to Use an API ?
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="lowerSide">
+          <div className="listItems">
+            <img src={home} alt="home" className="listitemsImg" />
+            Home
+          </div>
+          <div className="listItems">
+            <img src={saved} alt="saved" className="listitemsImg" />
+            Saved
+          </div>
+          <div className="listItems">
+            <img src={upgrade} alt="upgrade" className="listitemsImg" />
+            Upgrade
+          </div>
+        </div>
+      </div>
+      <div className="main"></div>
     </div>
   );
 }
